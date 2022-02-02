8f32cc3387ff929f35e6d720c8705f28d139f550
tacitvm <tacitvm@tuta.io>
2022-02-01 21:15:23 -0300

    change to custom cursor, navbar style and some website photo grids

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
- 
+//Navbar resizing on scroll
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+    document.getElementsByTagName('header').style.height = '8vh';
+    document.getElementById('web-name').style.height = '40%';
+}
+
+
+
+
+}
